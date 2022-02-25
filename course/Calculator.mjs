e2cfc8ed4d588d235fc0fe8d5143a7b94f846a3a
choiwonho <fhfwk9188@gmail.com>
2022-02-25 17:38:16 +0900

    3-3강

---
+++ course/Calculator.mjs
@@ -0,0 +1,37 @@
+import { question } from 'readline-sync';
+
+function showprompt() {
+    console.clear();
+    const firstNumber = question(' Enter first number : ');
+    const operation = question(' Enter operation : ');
+    const secondNumber = question(' Enter second number : ');
+
+    const validation = isNumber(firstNumber) && isOperation(operation) && isNumber(secondNumber) ;
+
+    if(validation) {
+        let result = calculate(parseInt(firstNumber), operation, parseInt(secondNumber));
+        console.log('답 : ' + result );
+    } else {
+        console.log('Invalid Input');
+    }
+}
+
+function isNumber( number ) {
+    return !isNaN(parseInt( number )) ;
+}
+
+function isOperation( operation ) {
+    let operations = '+-*/';
+    return operations.indexOf(operation) !== -1
+}
+
+function calculate (firstNumber, operation, secondNumber){
+    switch(operation) {
+        case '+' : return firstNumber + secondNumber ;
+        case '-' : return firstNumber - secondNumber ;
+        case '*' : return firstNumber * secondNumber ;
+        case '/' : return firstNumber / secondNumber ;
+    }
+}
+
+showprompt();
